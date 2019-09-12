5aaeb8706f65397d463a997b98bc35fb56ccedeb
wael <waelajaber@gmail.com>
2019-09-12 03:07:19 +0300

    working beta

--- MarkdownPreviewer/main.js
+++ MarkdownPreviewer/main.js
@@ -1,3 +1,4 @@
+
 // ALLOWS LINE BREAKS WITH RETURN BUTTON
 marked.setOptions({
     breaks: true,
@@ -9,20 +10,72 @@ renderer.link = function (href, title, text) {
     return `<a target="_blank" href="${href}">${text}` + '</a>';
 }
 
+// const converter = new showdown.Converter();
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub - heading...
+### And here's some other cool stuff:
+
+Heres some code, \`< div ></div > \`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            defaultText: '# Marked in the browser\n\nRendered by **marked**.',
-            text: '',
-        }
+            text: placeholder,
+        };
         this.handleChange = this.handleChange.bind(this);
+        // this.handleMarkdown = this.handleMarkdown.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({ markDown: marked(this.state.text, { renderer: renderer }) })
     }
 
     handleChange(e) {
-        this.setState(
-            { text: e.target.value }
-        )
+        this.setState({
+            text: e.target.value,
+        })
     }
 
     render() {
@@ -46,7 +99,7 @@ class App extends React.Component {
                                 <div className="row ">
                                     <textarea
                                         id="editor"
-                                        value={this.state.text.length === 0 ? this.state.defaultText : this.state.text}
+                                        value={this.state.text}
                                         onChange={this.handleChange}
                                         className="w-100 shadow border border-dark rounded rounded-lg mt-2 p-2" />
                                 </div>
@@ -66,12 +119,7 @@ class App extends React.Component {
                                     </div>
                                 </div>
 
-                                <div
-                                    id="preview"
-                                    className="row shadow border border-dark rounded mt-2 p-2" dangerouslySetInnerHTML=
-                                    {{ __html: this.state.text.length === 0 ? marked(this.state.defaultText) : marked(this.state.text) }}>
-
-                                </div>
+                                <div className="w-100 shadow border border-dark rounded rounded-lg mt-2 p-2" id='preview' dangerouslySetInnerHTML={{ __html: marked(this.state.text, { renderer: renderer }) }} />
 
                             </div>
                         </div>
@@ -83,6 +131,7 @@ class App extends React.Component {
         )
     }
 }
+
 
 const domContainer = document.getElementById('app-container');
 ReactDOM.render(<App />, domContainer);
