fff1d33fbf86fd9a8aa5bde618981a1facced1ce
wael <waelajaber@gmail.com>
2019-09-19 22:54:41 +0300

    .

--- Calculator/main.js
+++ Calculator/main.js
@@ -2,40 +2,62 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            stack: '',
+        };
 
-        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleClear = this.handleClear.bind(this);
+        this.handleDel = this.handleDel.bind(this);
     }
 
+    handleInput(e) {
+        this.setState({
+            stack: this.state.stack.concat(e.target.innerHTML),
+        })
+    }
+
+    handleClear() {
+        this.setState({ stack: '' })
+    }
+
+    handleDel() {
+        let current = this.state.stack;
+        let newStack = current.slice(0, current.length - 1);
+        this.setState({
+            stack: newStack
+        })
+    }
 
     render() {
+        console.log(this.state.stack)
         return (
             <React.Fragment>
                 <div id="calculator-container">
                     <div id="display">
-                        hello motherfuckers
+                        {this.state.stack}
                     </div>
                     <div id="pad">
                         <div id="number-container">
-                            <button className="numbers" id="seven">7</button>
-                            <button className="numbers" id="eight">8</button>
-                            <button className="numbers" id="nine">9</button>
-                            <button className="numbers" id="four">4</button>
-                            <button className="numbers" id="five">5</button>
-                            <button className="numbers" id="six">6</button>
-                            <button className="numbers" id="one">1</button>
-                            <button className="numbers" id="two">2</button>
-                            <button className="numbers" id="three">3</button>
-                            <button className="numbers" id="zero">0</button>
-                            <button className="numbers" id="decimal">.</button>
+                            <button onClick={this.handleInput} className="numbers" id="seven">7</button>
+                            <button onClick={this.handleInput} className="numbers" id="eight">8</button>
+                            <button onClick={this.handleInput} className="numbers" id="nine">9</button>
+                            <button onClick={this.handleInput} className="numbers" id="four">4</button>
+                            <button onClick={this.handleInput} className="numbers" id="five">5</button>
+                            <button onClick={this.handleInput} className="numbers" id="six">6</button>
+                            <button onClick={this.handleInput} className="numbers" id="one">1</button>
+                            <button onClick={this.handleInput} className="numbers" id="two">2</button>
+                            <button onClick={this.handleInput} className="numbers" id="three">3</button>
+                            <button onClick={this.handleInput} className="numbers" id="zero">0</button>
+                            <button onClick={this.handleInput} className="numbers" id="decimal">.</button>
                             <button className="numbers" id="equals">=</button>
                         </div>
                         <div id="ops">
-                            <i id="toggle" className="fa fa-toggle-on"></i>
-                            <button className="numbers" id="clear">AC</button>
-                            <button className="numbers" id="multiply">x</button>
-                            <button className="numbers" id="divide">/</button>
-                            <button className="numbers" id="add">+</button>
-                            <button className="numbers" id="subtract">-</button>
+                            <button onClick={this.handleDel} className="numbers" id="clear">Del</button>
+                            <button onClick={this.handleClear} className="numbers" id="clear">AC</button>
+                            <button onClick={this.handleInput} className="numbers" id="multiply">x</button>
+                            <button onClick={this.handleInput} className="numbers" id="divide">/</button>
+                            <button onClick={this.handleInput} className="numbers" id="add">+</button>
+                            <button onClick={this.handleInput} className="numbers" id="subtract">-</button>
                         </div>
                     </div>
                 </div>
